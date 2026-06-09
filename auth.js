c912b836e0106b3626275b4077b4372bdbf28a14
raz.sharaby <razsharaby@gmail.com>
2026-06-10 00:26:44 +0300

    fix: conditional GoogleSignin load for Expo Go compatibility

--- auth.js
+++ auth.js
@@ -1,6 +1,5 @@
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
-import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import { initializeApp } from "firebase/app";
 import {
   initializeAuth,
@@ -15,7 +14,21 @@ import {
 } from "firebase/auth";
 import { Platform } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import Constants, { ExecutionEnvironment } from "expo-constants";
 
+const isExpoGo =
+  Constants.executionEnvironment === ExecutionEnvironment.StoreClient;
+let GoogleSignin;
+if (!isExpoGo && Platform.OS === "android") {
+  GoogleSignin =
+    require("@react-native-google-signin/google-signin").GoogleSignin;
+  GoogleSignin.configure({
+    webClientId:
+      "878270972278-22344lcv57s76up31nc8iauiolmv3t3f.apps.googleusercontent.com",
+    androidClientId:
+      "56065825946-gcje8b2m6aaqskg3ien48t6gapdgbust.apps.googleusercontent.com",
+  });
+}
 WebBrowser.maybeCompleteAuthSession();
 
 const firebaseConfig = {
@@ -29,13 +42,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-GoogleSignin.configure({
-  webClientId:
-    "878270972278-22344lcv57s76up31nc8iauiolmv3t3f.apps.googleusercontent.com",
-  androidClientId:
-    "56065825946-gcje8b2m6aaqskg3ien48t6gapdgbust.apps.googleusercontent.com",
-});
-
 const auth =
   Platform.OS === "web"
     ? getAuth(app)
@@ -44,6 +50,7 @@ const auth =
       });
 
 export async function signInWithGoogleAndroid() {
+  if (!GoogleSignin) throw new Error("GoogleSignin not available");
   await GoogleSignin.hasPlayServices();
   const userInfo = await GoogleSignin.signIn();
   const { idToken } = userInfo.data;
